95532e86536f99a9bf16f53b0a6a31381b1a8347
Chris Brown <jazztone@Chriss-iMac.local>
2022-07-30 15:53:59 -0700

    added file

---
+++ rest-spread-exercises.js
@@ -0,0 +1,84 @@
+// function filterOutOdds() {
+//     var nums = Array.prototype.slice.call(arguments);
+//     return nums.filter(function (num) {
+//         return num % 2 === 0
+//     });
+// }
+
+/* Write an ES2015 Version */
+
+const filterOutOdds = (...nums) => nums.filter(num => num % 2 === 0);
+
+
+// Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
+
+const findMin = (...args) => Math.min(...args);
+
+// Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+
+const mergeObjects = (obj1, obj2) => ({ ...obj1, ...obj2 });
+
+// Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
+
+const doubleAndReturnArgs = (arr, ...args) => {
+    const doubled = args.map((val) => val * 2);
+    return [...arr, ...doubled];
+};
+
+// For this section, write the following functions using rest, spread and refactor these functions to be arrow functions!
+
+// Make sure that you are always returning a new array or object and not modifying the existing inputs.
+
+/** remove a random element in the items array
+and return a new array without that item. */
+
+const removeRandom = items => {
+    const randomNum = Math.floor(Math.random() * items.length);
+    const newItems = [...items];
+    newItems.splice(randomNum, 1);
+    return newItems;
+};
+
+/** Return a new array with every item in array1 and array2. */
+
+const extend = (array1, array2) => [...array1, ...array2];
+
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+const addKeyVal = (obj, key, val) => {
+    const newObj = { ...obj };
+    newObj[key] = val;
+    return newObj;
+};
+
+
+/** Return a new object with a key removed. */
+
+const removeKey = (obj, key) => {
+    const newObj = { ...obj };
+    delete newObj[key];
+    return newObj;
+};
+
+
+
+
+/** Combine two objects and return a new object. */
+
+const combine = (obj1, obj2) => ({ ...obj1, ...obj2 });
+
+
+
+/** Return a new object with a modified key and value. */
+
+const update = (obj, key, val) => {
+    const newObj = { ...obj };
+    newObj[key] = val;
+    return newObj;
+}
+
+// when you define a function, do you need semicolon at end?
+// when you define a function, can you put an arrow function inside?
+
